529b9fb5cdb634def6575643c1f202db4ef7e6c4
Davi Luna <Sdavi738@gmail.com>
2023-03-08 09:04:29 -0300

    consertando uma besteira

--- back-end/add-contatos.php
+++ back-end/add-contatos.php
@@ -12,6 +12,6 @@
     $numero = $_POST['numero_mostrado'];
     $user = $_SESSION['user']['USR_USER_ID'];
 
-    $stmt = $conn->prepare('INSERT INTO CONT_contatos(CONT_NOME, CONT_ENDERECO, CONT_EMAIL , CONT_NUMERO, CONT_USER_ID,USR_USER_ID) VALUES (?, ? , ? , ?,?,USR_USER_ID)');
+    $stmt = $conn->prepare('INSERT INTO CONT_contatos(CONT_NOME, CONT_ENDERECO, CONT_EMAIL , CONT_NUMERO, CONT_USER_ID,USR_CONT_FK) VALUES (?, ? , ? , ?,?,USR_CONT_FK)');
     $stmt->execute([$name, $endereco, $email, $numero, $user ]);
 ?>
--- back-end/main.php
+++ back-end/main.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: http://localhost:5173');
     header('Access-Control-Allow-Credentials: true');
 
-    $dsn = 'mysql:dbname=WEB1_CONTATOS;host=127.0.0.1;port=3306';
+    $dsn = 'mysql:dbname=WEB1_CONTATOS;host=127.0.0.1;port=3307';
     $conn = new PDO($dsn, 'root', 'root');
 
     session_start();
